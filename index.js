64772bd0a4ddcffa63aa001ee75fed725c46b7eb
Mike Bostock <mbostock@gmail.com>
2016-03-03 10:39:11 -0800

    Simpler version export.

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
-import bundle from "./src/bundle";
-import cluster from "./src/cluster";
-import links from "./src/links";
-import pack from "./src/pack";
-import partition from "./src/partition";
-import tree from "./src/tree";
-import treemap from "./src/treemap";
-
-export {
-  bundle,
-  cluster,
-  links,
-  pack,
-  partition,
-  tree,
-  treemap
-};
+export {version} from "./build/version";
+export {default as bundle} from "./src/bundle";
+export {default as cluster} from "./src/cluster";
+export {default as links} from "./src/links";
+export {default as pack} from "./src/pack";
+export {default as partition} from "./src/partition";
+export {default as tree} from "./src/tree";
+export {default as treemap} from "./src/treemap";
