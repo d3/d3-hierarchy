5f2f35615860bbec0e73c711c1d772da5b047414
Mike Bostock <mbostock@gmail.com>
2016-03-14 17:11:26 -0700

    Checkpoint treemap.

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 export {version} from "./build/package";
 export {default as hierarchy} from "./src/hierarchy";
+export {default as treemap} from "./src/treemap/index";
+export {default as treemapDice} from "./src/treemap/dice";
+export {default as treemapSlice} from "./src/treemap/slice";
+export {default as treemapSliceDice} from "./src/treemap/sliceDice";
---
+++ src/treemap/index.js
@@ -0,0 +1,32 @@
+import hierarchy, {rebind} from "../hierarchy";
+import {visitBefore} from "../visit";
+import sliceDice from "./sliceDice";
+
+export default function() {
+  var layout = hierarchy(),
+      dx = 1, dy = 1,
+      tile = sliceDice;
+
+  function treemap(d) {
+    var nodes = layout(d),
+        root = nodes[0];
+    root.x0 = root.y0 = 0;
+    root.x1 = dx, root.y1 = dy;
+    visitBefore(root, function(node) {
+      if (node.children) {
+        tile(node, node.x0, node.y0, node.x1, node.y1);
+      }
+    });
+    return nodes;
+  }
+
+  treemap.size = function(x) {
+    return arguments.length ? (dx = +x[0], dy = +x[1], treemap) : [dx, dy];
+  };
+
+  treemap.tile = function(x) {
+    return arguments.length ? (tile = x, treemap) : tile;
+  };
+
+  return rebind(treemap, layout);
+}
