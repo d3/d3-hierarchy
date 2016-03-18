57e90ce112fd752d4db659bb8e632359fd2f426f
Mike Bostock <mbostock@gmail.com>
2016-03-17 17:55:03 -0700

    Fix pack overlap.
    If the front chain only has three circles, don’t try to remove any more!
    
    Related mbostock/d3#1761.

--- src/pack/pack.js
+++ src/pack/pack.js
@@ -80,7 +80,7 @@ function pack(circles) {
             break;
           }
         }
-        if (k === j) --sk;
+        if (k === j && !--sk) { a = a.next, b = b.next, --i; continue pack; }
         if (sj < sk || (sj === sk && b.circle.r < a.circle.r)) b = j;
         else a = k;
         a.next = b, b.previous = a, --i;
