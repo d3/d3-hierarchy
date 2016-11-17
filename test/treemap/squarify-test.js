b59d1f17b6ceb51a7919698033b2a770ddc08641
Mike Bostock <mbostock@gmail.com>
2016-11-17 14:15:26 -0800

    Add test for leading zero.

--- test/treemap/squarify-test.js
+++ test/treemap/squarify-test.js
@@ -105,3 +105,32 @@ tape("treemapSquarify(parent, x0, y0, x1, y1) handles a degenerate wide empty pa
   ]);
   test.end();
 });
+
+tape("treemapSquarify(parent, x0, y0, x1, y1) handles a leading zero value", function(test) {
+  var tile = d3_hierarchy.treemapSquarify,
+      root = {
+        value: 24,
+        children: [
+          {value: 0},
+          {value: 6},
+          {value: 6},
+          {value: 4},
+          {value: 3},
+          {value: 2},
+          {value: 2},
+          {value: 1}
+        ]
+      };
+  tile(root, 0, 0, 6, 4);
+  test.deepEqual(root.children.map(round), [
+    {x0: 0.00, x1: 3.00, y0: 0.00, y1: 0.00},
+    {x0: 0.00, x1: 3.00, y0: 0.00, y1: 2.00},
+    {x0: 0.00, x1: 3.00, y0: 2.00, y1: 4.00},
+    {x0: 3.00, x1: 4.71, y0: 0.00, y1: 2.33},
+    {x0: 4.71, x1: 6.00, y0: 0.00, y1: 2.33},
+    {x0: 3.00, x1: 5.40, y0: 2.33, y1: 3.17},
+    {x0: 3.00, x1: 5.40, y0: 3.17, y1: 4.00},
+    {x0: 5.40, x1: 6.00, y0: 2.33, y1: 4.00}
+  ]);
+  test.end();
+});
