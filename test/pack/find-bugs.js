751e22259feac7fad4439917ca9b32f497848ae6
Mike Bostock <mbostock@gmail.com>
2017-02-07 14:33:19 -0800

    Add stress test.

---
+++ test/pack/find-bugs.js
@@ -0,0 +1,54 @@
+var d3 = require("../../");
+
+var n = 0, r = randomNormal();
+
+while (true) {
+  if (!(n % 100)) process.stdout.write(".");
+  if (!(n % 10000)) process.stdout.write("\n" + n + " ");
+  ++n;
+  var radii = [0, 1, 2, 3, 4].map(r).map(Math.abs);
+  if (intersectsAny(d3.packSiblings(radii.map(r => ({r: r}))))) {
+    process.stdout.write("\n");
+    process.stdout.write(JSON.stringify(radii));
+    process.stdout.write("\n");
+  }
+}
+
+function randomNormal(mu, sigma) {
+  var x, r;
+  mu = mu == null ? 0 : +mu;
+  sigma = sigma == null ? 1 : +sigma;
+  return function() {
+    var y;
+
+    // If available, use the second previously-generated uniform random.
+    if (x != null) y = x, x = null;
+
+    // Otherwise, generate a new x and y.
+    else do {
+      x = Math.random() * 2 - 1;
+      y = Math.random() * 2 - 1;
+      r = x * x + y * y;
+    } while (!r || r > 1);
+
+    return mu + sigma * y * Math.sqrt(-2 * Math.log(r) / r);
+  };
+}
+
+function intersectsAny(circles) {
+  for (var i = 0, n = circles.length; i < n; ++i) {
+    for (var j = i + 1, ci = circles[i], cj; j < n; ++j) {
+      if (intersects(ci, cj = circles[j])) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function intersects(a, b) {
+  var dx = b.x - a.x,
+      dy = b.y - a.y,
+      dr = a.r + b.r;
+  return dr * dr - 1e-6 > dx * dx + dy * dy;
+}
