39a5228c48880e9a8ed6e464a212829edc3e9e00
Mike Bostock <mbostock@gmail.com>
2017-05-25 13:40:26 -0700

    Local style tweaks.

--- src/pack/enclose.js
+++ src/pack/enclose.js
@@ -5,7 +5,7 @@ export default function(circles) {
 function findBasis(L, n, B) {
   var circle = encloseBasis(B);
 
-  for (var i = 0; i < n; i++) {
+  for (var i = 0; i < n; ++i) {
     var p = L[i];
     if (!circle || !encloses(circle, p)) {
       B = findBasis(L, i + 1, extendBasis(B, p));
@@ -17,7 +17,7 @@ function findBasis(L, n, B) {
 }
 
 function encloseBasis(B) {
-  switch(B.length) {
+  switch (B.length) {
     case 1: return enclose1(B[0]);
     case 2: return enclose2(B[0], B[1]);
     case 3: return enclose3(B[0], B[1], B[2]);
@@ -28,34 +28,30 @@ function extendBasis(B, p) {
   var i, j;
   if (enclosesAll(p, B)) return [p];
 
-  // If we get here then B must have at least one element
-  for (i = 0; i < B.length; i++) {
-    if (enclosesAll(enclose2(B[i], p), B)) return [B[i], p];
-  }
+  // If we get here then B must have at least one element.
+  for (i = 0; i < B.length; ++i) if (enclosesAll(enclose2(B[i], p), B)) return [B[i], p];
 
-  // If we get here then B must have at least two elements
-  for (i = 0; i < B.length-1; i++) {
-    for (j = i+1; j < B.length; j++) {
+  // If we get here then B must have at least two elements.
+  for (i = 0; i < B.length - 1; ++i) {
+    for (j = i + 1; j < B.length; ++j) {
       if (isBasis(B[i], B[j], p) && enclosesAll(enclose3(B[i], B[j], p), B)) {
         return [B[i], B[j], p];
       }
     }
   }
 
-  // If we get here then something is very wrong
-  throw new Error("extendBasis: we should never get here");
+  // If we get here then something is very wrong.
+  throw new Error;
 }
 
 function isBasis(a, b, c) {
   return !encloses(enclose2(a, b), c)
-    && !encloses(enclose2(a, c), b)
-    && !encloses(enclose2(b, c), a);
+      && !encloses(enclose2(a, c), b)
+      && !encloses(enclose2(b, c), a);
 }
 
 function enclosesAll(a, B) {
-  for (var i = 0; i < B.length; i++) {
-    if (!encloses(a, B[i])) return false;
-  }
+  for (var i = 0; i < B.length; ++i) if (!encloses(a, B[i])) return false;
   return true;
 }
 
