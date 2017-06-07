355cc0104b034382c493c56ffc5e0fc20da78c8f
Mike Bostock <mbostock@gmail.com>
2017-06-07 11:59:23 -0700

    Revert "Lazy shuffling."
    This reverts commit e66cf50d72bb062c5bcb1c1267e6a10d4f877c19.

--- src/pack/enclose.js
+++ src/pack/enclose.js
@@ -1,10 +1,10 @@
-import {slice} from "../array";
+import {shuffle, slice} from "../array";
 
-export default function(L) {
-  var i = 0, j, n = (L = slice.call(L)).length, B = [], p, e;
+export default function(circles) {
+  var i = 0, n = (circles = shuffle(slice.call(circles))).length, B = [], p, e;
 
   while (i < n) {
-    p = L[j = i + (Math.random() * (n - i) | 0)], L[j] = L[i], L[i] = p;
+    p = circles[i];
     if (e && enclosesWeak(e, p)) ++i;
     else e = encloseBasis(B = extendBasis(B, p)), i = 0;
   }
