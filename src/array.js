c8ddf26fa182fce1a4885ceb0bc82a80ea95973f
Mike Bostock <mbostock@gmail.com>
2017-06-07 08:41:30 -0700

    Remove unused variable.

--- src/array.js
+++ src/array.js
@@ -2,15 +2,14 @@ export var slice = Array.prototype.slice;
 
 export function shuffle(array) {
   var m = array.length,
-      i0 = 0,
       t,
       i;
 
   while (m) {
     i = Math.random() * m-- | 0;
-    t = array[m + i0];
-    array[m + i0] = array[i + i0];
-    array[i + i0] = t;
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
   }
 
   return array;
