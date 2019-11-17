b10e3b3811575d4a4e069a33bfcb57f1cfb1b63e
Mike Bostock <mbostock@gmail.com>
2019-11-16 17:33:05 -0800

    Remove unnecessary assertion.

--- src/hierarchy/find.js
+++ src/hierarchy/find.js
@@ -1,5 +1,4 @@
 export default function(callback, that) {
-  if (typeof callback !== "function") throw new Error("not a function");
   let index = -1;
   for (const node of this) {
     if (callback.call(that, node, ++index, this)) {
