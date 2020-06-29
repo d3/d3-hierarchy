5b5f41d19761528c73704ee12ba7e7d053a7c939
Menai Ala Eddine <ed.sheeran.eng@gmail.com>
2020-06-28 17:53:54 -0700

    Change function syntax.
    1. Separate variables declaration.
    2. Change to arrow syntax.
    3. Change if/else to short-circuit evaluation.

--- src/hierarchy/links.js
+++ src/hierarchy/links.js
@@ -1,9 +1,8 @@
 export default function() {
-  var root = this, links = [];
-  root.each(function(node) {
-    if (node !== root) { // Don’t include the root’s parent, if any.
-      links.push({source: node.parent, target: node});
-    }
+  const root = this;
+  const links = [];
+  root.each((node) => {
+    node !== root && links.push({source: node.parent, target: node});
   });
   return links;
 }
