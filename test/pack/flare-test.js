6ab84f89508c6f0778015fe2ac84e15fd8202a4b
Philippe Rivière <fil@rezo.net>
2020-07-27 17:15:41 +0200

    remove devDependency on d3-queue

--- test/pack/flare-test.js
+++ test/pack/flare-test.js
@@ -1,6 +1,5 @@
 var fs = require("fs"),
     tape = require("tape"),
-    d3_queue = require("d3-queue"),
     d3_dsv = require("d3-dsv"),
     d3_hierarchy = require("../../");
 
@@ -9,53 +8,46 @@ tape("pack(flare) produces the expected result", test(
   "test/data/flare-pack.json"
 ));
 
-function test(input, expected) {
+function test(inputFile, expectedFile) {
   return function(test) {
+    const inputText = fs.readFileSync(inputFile, "utf8"),
+        expectedText = fs.readFileSync(expectedFile, "utf8");
 
-    d3_queue.queue()
-        .defer(fs.readFile, input, "utf8")
-        .defer(fs.readFile, expected, "utf8")
-        .await(ready);
+    var stratify = d3_hierarchy.stratify()
+        .parentId(function(d) { var i = d.id.lastIndexOf("."); return i >= 0 ? d.id.slice(0, i) : null; });
 
-    function ready(error, inputText, expectedText) {
-      if (error) throw error;
+    var pack = d3_hierarchy.pack()
+        .size([960, 960]);
 
-      var stratify = d3_hierarchy.stratify()
-          .parentId(function(d) { var i = d.id.lastIndexOf("."); return i >= 0 ? d.id.slice(0, i) : null; });
+    var data = d3_dsv.csvParse(inputText),
+        expected = JSON.parse(expectedText),
+        actual = pack(stratify(data)
+            .sum(function(d) { return d.value; })
+            .sort(function(a, b) { return b.value - a.value || a.data.id.localeCompare(b.data.id); }));
 
-      var pack = d3_hierarchy.pack()
-          .size([960, 960]);
+    (function visit(node) {
+      node.name = node.data.id.slice(node.data.id.lastIndexOf(".") + 1);
+      node.x = round(node.x);
+      node.y = round(node.y);
+      node.r = round(node.r);
+      delete node.id;
+      delete node.parent;
+      delete node.data;
+      delete node.depth;
+      delete node.height;
+      if (node.children) node.children.forEach(visit);
+    })(actual);
 
-      var data = d3_dsv.csvParse(inputText),
-          expected = JSON.parse(expectedText),
-          actual = pack(stratify(data)
-              .sum(function(d) { return d.value; })
-              .sort(function(a, b) { return b.value - a.value || a.data.id.localeCompare(b.data.id); }));
+    (function visit(node) {
+      node.x = round(node.x);
+      node.y = round(node.y);
+      node.r = round(node.r);
+      if (node.children) node.children.forEach(visit);
+    })(expected);
 
-      (function visit(node) {
-        node.name = node.data.id.slice(node.data.id.lastIndexOf(".") + 1);
-        node.x = round(node.x);
-        node.y = round(node.y);
-        node.r = round(node.r);
-        delete node.id;
-        delete node.parent;
-        delete node.data;
-        delete node.depth;
-        delete node.height;
-        if (node.children) node.children.forEach(visit);
-      })(actual);
-
-      (function visit(node) {
-        node.x = round(node.x);
-        node.y = round(node.y);
-        node.r = round(node.r);
-        if (node.children) node.children.forEach(visit);
-      })(expected);
-
-      test.deepEqual(actual, expected);
-      test.end();
-    }
-  };
+    test.deepEqual(actual, expected);
+    test.end();
+  }
 }
 
 function round(x) {
--- test/treemap/flare-test.js
+++ test/treemap/flare-test.js
@@ -1,6 +1,5 @@
 var fs = require("fs"),
     tape = require("tape"),
-    d3_queue = require("d3-queue"),
     d3_dsv = require("d3-dsv"),
     d3_hierarchy = require("../../");
 
@@ -16,63 +15,56 @@ tape("treemap(flare) produces the expected result with a squarified ratio of 1",
   d3_hierarchy.treemapSquarify.ratio(1)
 ));
 
-function test(input, expected, tile) {
+function test(inputFile, expectedFile, tile) {
   return function(test) {
+    const inputText = fs.readFileSync(inputFile, "utf8"),
+        expectedText = fs.readFileSync(expectedFile, "utf8");
 
-    d3_queue.queue()
-        .defer(fs.readFile, input, "utf8")
-        .defer(fs.readFile, expected, "utf8")
-        .await(ready);
+    var stratify = d3_hierarchy.stratify()
+        .parentId(function(d) { var i = d.id.lastIndexOf("."); return i >= 0 ? d.id.slice(0, i) : null; });
 
-    function ready(error, inputText, expectedText) {
-      if (error) throw error;
+    var treemap = d3_hierarchy.treemap()
+        .tile(tile)
+        .size([960, 500]);
 
-      var stratify = d3_hierarchy.stratify()
-          .parentId(function(d) { var i = d.id.lastIndexOf("."); return i >= 0 ? d.id.slice(0, i) : null; });
+    var data = d3_dsv.csvParse(inputText),
+        expected = JSON.parse(expectedText),
+        actual = treemap(stratify(data)
+            .sum(function(d) { return d.value; })
+            .sort(function(a, b) { return b.value - a.value || a.data.id.localeCompare(b.data.id); }));
 
-      var treemap = d3_hierarchy.treemap()
-          .tile(tile)
-          .size([960, 500]);
+    (function visit(node) {
+      node.name = node.data.id.slice(node.data.id.lastIndexOf(".") + 1);
+      node.x0 = round(node.x0);
+      node.y0 = round(node.y0);
+      node.x1 = round(node.x1);
+      node.y1 = round(node.y1);
+      delete node.id;
+      delete node.parent;
+      delete node.data;
+      delete node._squarify;
+      delete node.height;
+      if (node.children) node.children.forEach(visit);
+    })(actual);
 
-      var data = d3_dsv.csvParse(inputText),
-          expected = JSON.parse(expectedText),
-          actual = treemap(stratify(data)
-              .sum(function(d) { return d.value; })
-              .sort(function(a, b) { return b.value - a.value || a.data.id.localeCompare(b.data.id); }));
+    (function visit(node) {
+      node.x0 = round(node.x);
+      node.y0 = round(node.y);
+      node.x1 = round(node.x + node.dx);
+      node.y1 = round(node.y + node.dy);
+      delete node.x;
+      delete node.y;
+      delete node.dx;
+      delete node.dy;
+      if (node.children) {
+        node.children.reverse(); // D3 3.x bug
+        node.children.forEach(visit);
+      }
+    })(expected);
 
-      (function visit(node) {
-        node.name = node.data.id.slice(node.data.id.lastIndexOf(".") + 1);
-        node.x0 = round(node.x0);
-        node.y0 = round(node.y0);
-        node.x1 = round(node.x1);
-        node.y1 = round(node.y1);
-        delete node.id;
-        delete node.parent;
-        delete node.data;
-        delete node._squarify;
-        delete node.height;
-        if (node.children) node.children.forEach(visit);
-      })(actual);
-
-      (function visit(node) {
-        node.x0 = round(node.x);
-        node.y0 = round(node.y);
-        node.x1 = round(node.x + node.dx);
-        node.y1 = round(node.y + node.dy);
-        delete node.x;
-        delete node.y;
-        delete node.dx;
-        delete node.dy;
-        if (node.children) {
-          node.children.reverse(); // D3 3.x bug
-          node.children.forEach(visit);
-        }
-      })(expected);
-
-      test.deepEqual(actual, expected);
-      test.end();
-    }
-  };
+    test.deepEqual(actual, expected);
+    test.end();
+  }
 }
 
 function round(x) {
