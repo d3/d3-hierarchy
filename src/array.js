1f6e4c47f786f66bd1565801a5e3b6c317853df5
Philippe Rivière <fil@rezo.net>
2022-04-01 19:03:02 +0200

    inline lcg
    fixes https://github.com/d3/d3-hierarchy/pull/190#issuecomment-1086121332

--- src/array.js
+++ src/array.js
@@ -1,19 +1,23 @@
-import lcg from "./lcg.js";
-
 export default function(x) {
   return typeof x === "object" && "length" in x
     ? x // Array, TypedArray, NodeList, array-like
     : Array.from(x); // Map, Set, iterable, string, or anything else
 }
 
+// https://en.wikipedia.org/wiki/Linear_congruential_generator#Parameters_in_common_use
+const a = 1664525;
+const c = 1013904223;
+const mm = 4294967296; // 2^32
+
 export function shuffle(array) {
-  const random = lcg();
+  let s = 1;
   let m = array.length,
       t,
       i;
 
   while (m) {
-    i = random() * m-- | 0;
+    s = (a * s + c) % mm;
+    i = s / mm * m-- | 0;
     t = array[m];
     array[m] = array[i];
     array[i] = t;
